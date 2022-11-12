098198ef85868d59995bc2de25d1b61571ba4b36
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2022-11-12 15:35:13 +0200

    improve types, expose ddpError

--- livedata_server.ts
+++ livedata_server.ts
@@ -1,3 +1,4 @@
 export { DDPServer } from "./meteor/ddp/livedata_server";
+export { ddpError } from "./meteor/ddp/livedata_server";
 export { LiveMongoConnection } from "./meteor/mongo/live_connection";
 export { LiveCursor } from "./meteor/mongo/live_cursor";
